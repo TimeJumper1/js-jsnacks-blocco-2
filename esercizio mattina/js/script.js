191e67387c679ef2dcb4d43241c27b7929d1c366
TimeJumper1 <benassai.stefano.1@gmail.com>
2021-11-19 18:31:58 +0100

    esercizio mattina

---
+++ esercizio mattina/js/script.js
@@ -0,0 +1,9 @@
+
+let chosenNumber;
+chosenNumber = parseInt(prompt('inserisci un numero'));
+if (chosenNumber % 2 == 0) {
+    alert(chosenNumber);
+}else {
+    chosenNumber++
+    alert(chosenNumber);
+}

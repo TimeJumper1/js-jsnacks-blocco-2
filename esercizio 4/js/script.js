89aa509e3863fbe928a35a410d587f21d889c9a2
TimeJumper1 <benassai.stefano.1@gmail.com>
2021-11-19 17:12:41 +0100

    esercizio 4

--- esercizio 4/js/script.js
+++ esercizio 4/js/script.js
@@ -1,8 +1,12 @@
-let chosenNumber;
-chosenNumber = parseInt(prompt('inserisci un numero'));
-if (chosenNumber % 2 == 0) {
-    alert(chosenNumber);
-}else {
-    chosenNumber++
-    alert(chosenNumber);
+let arrayNew = []
+let arraySomma = 0
+while ( arraySomma < 50){ 
+    if ( arraySomma < 50 ){
+        userNumber = parseInt(prompt('inserisci un numero'))
+        arrayNew.push(userNumber);
+        arraySomma += userNumber
+    }
 }
+
+console.log(arrayNew)
+console.log(arraySomma)

2753e2ca41ac92c1979503b3c81fee0b3ee3fb78
TimeJumper1 <benassai.stefano.1@gmail.com>
2021-11-19 17:02:22 +0100

    esercizio 3

--- esercizio 3/js/script.js
+++ esercizio 3/js/script.js
@@ -1,8 +1,12 @@
-let chosenNumber;
-chosenNumber = parseInt(prompt('inserisci un numero'));
-if (chosenNumber % 2 == 0) {
-    alert(chosenNumber);
-}else {
-    chosenNumber++
-    alert(chosenNumber);
+const numbers = [1, 39 , 68 , 4 , 25 , 1 , 3 , 90 , 9 , 11 , 32 , 0 , 21 , 5 ,7 ,7 ,8 ,10 ];
+
+console.log(numbers);
+let sommaOdd = 0
+for (i = 0; i < numbers.length; i++){
+    
+    if (i % 2 !== 0){
+        const oddNumber= numbers[i];
+        sommaOdd += oddNumber ;
+    };
 }
+alert(`la somma dei numeri in posizione dispari è ${sommaOdd}`)

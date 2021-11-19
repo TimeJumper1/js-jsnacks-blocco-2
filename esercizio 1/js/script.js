bdcb7324f927951b4b2c17b86d0799cb5479caf0
TimeJumper1 <benassai.stefano.1@gmail.com>
2021-11-19 15:32:00 +0100

    esercizio 1

---
+++ esercizio 1/js/script.js
@@ -0,0 +1,8 @@
+let chosenNumber;
+chosenNumber = parseInt(prompt('inserisci un numero'));
+if (chosenNumber % 2 == 0) {
+    alert(chosenNumber);
+}else {
+    chosenNumber++
+    alert(chosenNumber);
+}
